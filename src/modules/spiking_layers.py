43910e5d29505d328827ab426a0659b377527a4b
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-04-03 09:28:14 -0400

    Update spiking_layers.py

--- src/modules/spiking_layers.py
+++ src/modules/spiking_layers.py
@@ -1,5 +1,5 @@
 import enum
-from typing import NamedTuple, Tuple, Type
+from typing import Tuple, Type
 
 import numpy as np
 import torch
@@ -11,6 +11,7 @@ from src.modules.spike_funcs import HeavisideSigmoidApprox, SpikeFunction
 class LayerType(enum.Enum):
 	LIF = enum.auto()
 	ALIF = enum.auto()
+	Izhikevich = enum.auto()
 
 
 class RNNLayer(torch.nn.Module):
@@ -19,6 +20,7 @@ class RNNLayer(torch.nn.Module):
 			input_size: int,
 			output_size: int,
 			use_recurrent_connection=True,
+			use_rec_eye_mask=True,
 			dt=1e-3,
 			device=None,
 			**kwargs
@@ -34,6 +36,26 @@ class RNNLayer(torch.nn.Module):
 		self.dt = dt
 		self.kwargs = kwargs
 		self._set_default_kwargs()
+
+		self.forward_weights = nn.Parameter(
+			torch.empty((self.input_size, self.output_size), device=self.device, dtype=torch.float32),
+			requires_grad=True
+		)
+		self.use_rec_eye_mask = use_rec_eye_mask
+		if use_recurrent_connection:
+			self.recurrent_weights = nn.Parameter(
+				torch.empty((self.output_size, self.output_size), device=self.device, dtype=torch.float32),
+				requires_grad=True
+			)
+			if use_rec_eye_mask:
+				self.rec_mask = (1 - torch.eye(self.output_size, device=self.device, dtype=torch.float32))
+			else:
+				self.rec_mask = torch.ones(
+					(self.output_size, self.output_size), device=self.device,  dtype=torch.float32
+				)
+		else:
+			self.recurrent_weights = None
+			self.rec_mask = None
 
 	def _set_default_kwargs(self):
 		raise NotImplementedError()
@@ -71,25 +93,15 @@ class RNNLayer(torch.nn.Module):
 				torch.nn.init.normal_(param)
 
 
-class LIFState(NamedTuple):
-	"""
-	V: membrane potential
-	Z: spikes
-	"""
-	I: torch.Tensor
-	V: torch.Tensor
-	Z: torch.Tensor
-
-
 class LIFLayer(RNNLayer):
 	def __init__(
 			self,
 			input_size: int,
 			output_size: int,
 			use_recurrent_connection=True,
+			use_rec_eye_mask=True,
 			spike_func: Type[SpikeFunction] = HeavisideSigmoidApprox,
 			dt=1e-3,
-			use_rec_eye_mask=True,
 			device=None,
 			**kwargs
 	):
@@ -98,28 +110,11 @@ class LIFLayer(RNNLayer):
 			input_size=input_size,
 			output_size=output_size,
 			use_recurrent_connection=use_recurrent_connection,
+			use_rec_eye_mask=use_rec_eye_mask,
 			dt=dt,
 			device=device,
 			**kwargs
 		)
-
-		self.forward_weights = nn.Parameter(
-			torch.empty((self.input_size, self.output_size), device=self.device, dtype=torch.float32),
-			requires_grad=True
-		)
-		self.use_rec_eye_mask = use_rec_eye_mask
-		if use_recurrent_connection:
-			self.recurrent_weights = nn.Parameter(
-				torch.empty((self.output_size, self.output_size), device=self.device, dtype=torch.float32),
-				requires_grad=True
-			)
-			if use_rec_eye_mask:
-				self.rec_mask = (1 - torch.eye(self.output_size, device=self.device, dtype=torch.float32))
-			else:
-				self.rec_mask = torch.ones((self.output_size, self.output_size), device=self.device, dtype=torch.float32)
-		else:
-			self.recurrent_weights = None
-			self.rec_mask = None
 
 		self.alpha = torch.tensor(np.exp(-dt / self.kwargs["tau_m"]), dtype=torch.float32, device=self.device)
 		self.threshold = torch.tensor(self.kwargs["threshold"], dtype=torch.float32, device=self.device)
@@ -182,9 +177,9 @@ class ALIFLayer(LIFLayer):
 			input_size: int,
 			output_size: int,
 			use_recurrent_connection=True,
+			use_rec_eye_mask=True,
 			spike_func: Type[SpikeFunction] = HeavisideSigmoidApprox,
 			dt=1e-3,
-			use_rec_eye_mask=True,
 			device=None,
 			**kwargs
 	):
@@ -192,9 +187,9 @@ class ALIFLayer(LIFLayer):
 			input_size=input_size,
 			output_size=output_size,
 			use_recurrent_connection=use_recurrent_connection,
+			use_rec_eye_mask=use_rec_eye_mask,
 			spike_func=spike_func,
 			dt=dt,
-			use_rec_eye_mask=use_rec_eye_mask,
 			device=device,
 			**kwargs
 		)
@@ -245,13 +240,114 @@ class ALIFLayer(LIFLayer):
 		return next_Z, (next_V, next_a, next_Z)
 
 
-class ReadoutState(NamedTuple):
+class IzhikevichLayer(RNNLayer):
 	"""
-	I: current
-	V: membrane potential
+	Izhikevich p.274
+
 	"""
-	I: torch.Tensor
-	V: torch.Tensor
+	def __init__(
+			self,
+			input_size: int,
+			output_size: int,
+			use_recurrent_connection=True,
+			use_rec_eye_mask=True,
+			spike_func: Type[SpikeFunction] = HeavisideSigmoidApprox,
+			dt=1e-3,
+			device=None,
+			**kwargs
+	):
+		self.spike_func = spike_func
+		super(IzhikevichLayer, self).__init__(
+			input_size=input_size,
+			output_size=output_size,
+			use_recurrent_connection=use_recurrent_connection,
+			use_rec_eye_mask=use_rec_eye_mask,
+			dt=dt,
+			device=device,
+			**kwargs
+		)
+
+		self.C = torch.tensor(self.kwargs["C"], dtype=torch.float32, device=self.device)
+		self.v_rest = torch.tensor(self.kwargs["v_rest"], dtype=torch.float32, device=self.device)
+		self.v_th = torch.tensor(self.kwargs["v_th"], dtype=torch.float32, device=self.device)
+		self.k = torch.tensor(self.kwargs["k"], dtype=torch.float32, device=self.device)
+		self.a = torch.tensor(self.kwargs["a"], dtype=torch.float32, device=self.device)
+		self.b = torch.tensor(self.kwargs["b"], dtype=torch.float32, device=self.device)
+		self.c = torch.tensor(self.kwargs["c"], dtype=torch.float32, device=self.device)
+		self.d = torch.tensor(self.kwargs["d"], dtype=torch.float32, device=self.device)
+		self.v_peak = torch.tensor(self.kwargs["v_peak"], dtype=torch.float32, device=self.device)
+		self.gamma = torch.tensor(self.kwargs["gamma"], dtype=torch.float32, device=self.device)
+		self.initialize_weights_()
+
+	def _set_default_kwargs(self):
+		self.kwargs.setdefault("C", 100.0)
+		self.kwargs.setdefault("v_rest", -60.0)
+		self.kwargs.setdefault("v_th", -40.0)
+		self.kwargs.setdefault("k", 0.7)
+		self.kwargs.setdefault("a", 0.03)
+		self.kwargs.setdefault("b", -2.0)
+		self.kwargs.setdefault("c", -50.0)
+		self.kwargs.setdefault("d", 100.0)
+		self.kwargs.setdefault("v_peak", 35.0)
+		if isinstance(self.spike_func, HeavisideSigmoidApprox):
+			self.kwargs.setdefault("gamma", 100.0)
+		else:
+			self.kwargs.setdefault("gamma", 1.0)
+
+	def initialize_weights_(self):
+		gain = 1.0
+		for param in self.parameters():
+			if param.ndim > 2:
+				torch.nn.init.xavier_normal_(param, gain=gain)
+			else:
+				torch.nn.init.normal_(param, std=gain)
+
+	def create_empty_state(self, batch_size: int = 1) -> Tuple[torch.Tensor, ...]:
+		"""
+		Create an empty state in the following form:
+			([membrane potential of shape (batch_size, self.output_size)],
+			[membrane potential of shape (batch_size, self.output_size)],
+			[spikes of shape (batch_size, self.output_size)])
+		:param batch_size: The size of the current batch.
+		:return: The current state.
+		"""
+		V = self.v_rest * torch.ones(
+			(batch_size, self.output_size),
+			device=self.device,
+			dtype=torch.float32,
+			requires_grad=True,
+		)
+		u = torch.zeros(
+			(batch_size, self.output_size),
+			device=self.device,
+			dtype=torch.float32,
+			requires_grad=True,
+		)
+		Z = torch.zeros(
+			(batch_size, self.output_size),
+			device=self.device,
+			dtype=torch.float32,
+			requires_grad=True,
+		)
+		return V, u, Z
+
+	def forward(self, inputs: torch.Tensor, state: Tuple[torch.Tensor, ...] = None):
+		assert inputs.ndim == 2
+		batch_size, nb_features = inputs.shape
+		V, u, Z = self._init_forward_state(state, batch_size)
+		input_current = torch.matmul(inputs, self.forward_weights)
+		if self.use_recurrent_connection:
+			rec_current = torch.matmul(Z, torch.mul(self.recurrent_weights, self.rec_mask))
+		else:
+			rec_current = 0.0
+		is_reset = Z.detach()
+		I = input_current + rec_current
+		dVdt = self.k * (V - self.v_rest) * (V - self.v_th) - u + I
+		next_V = (V + self.dt * dVdt / self.C) * (1.0 - is_reset) + self.c * is_reset
+		dudt = self.a * (self.b * (V - self.v_rest) - u)
+		next_u = (u + self.dt * dudt) + self.d * is_reset
+		next_Z = self.spike_func.apply(next_V, self.v_peak, self.gamma)
+		return next_Z, (next_V, next_u, next_Z)
 
 
 class ReadoutLayer(RNNLayer):
@@ -270,10 +366,6 @@ class ReadoutLayer(RNNLayer):
 			dt=dt,
 			device=device,
 			**kwargs
-		)
-		self.forward_weights = nn.Parameter(
-			torch.empty((self.input_size, self.output_size), device=self.device, dtype=torch.float32),
-			requires_grad=True,
 		)
 		self.bias_weights = nn.Parameter(
 			torch.empty((self.output_size,), device=self.device),
@@ -316,5 +408,6 @@ class ReadoutLayer(RNNLayer):
 LayerType2Layer = {
 	LayerType.LIF: LIFLayer,
 	LayerType.ALIF: ALIFLayer,
+	LayerType.Izhikevich: IzhikevichLayer,
 }
 
